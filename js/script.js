9254b97fe337997cf7ee8a38fe707892968cb2b1
danyllo leles <danyllolopes@outlook.com.br>
2022-06-27 11:50:15 -0300

    base do portfolio finalizada

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import { SlideNav } from "./slide.js";
+
+const slide = new SlideNav(".slide", ".wrapper");
+slide.init();
+slide.addArrow(".prev", ".next");
+
+slide.addControl(".custom-controls");
